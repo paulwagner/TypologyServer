7391661aac08656dc26168506c2eb3fc3b8408ed
paulwagner <paul@typology.de>
2012-07-21 22:58:52 +0200

    Test case for class SBVEntry

---
+++ Typology/test/de/typology/AllTestsSuite.java
@@ -0,0 +1,10 @@
+package de.typology;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ de.typology.tools.AllTestsTools.class} )
+public final class AllTestsSuite {
+	
+}
