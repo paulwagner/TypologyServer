549d2df321c437d37e4ef8b6ac08a71b5ea6258e
paulwagner <paul@typology.de>
2012-08-05 23:28:22 +0200

    AbstractRequestCallback tests

--- Typology/test/de/typology/requests/AllTestsRequests.java
+++ Typology/test/de/typology/requests/AllTestsRequests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ RequestTest.class, RequestProcessorTest.class, RequestToolsTest.class })
+@SuiteClasses({ AbstractRequestCallbackTest.class, RequestTest.class, RequestProcessorTest.class, RequestToolsTest.class })
 public class AllTestsRequests {
 
 }
