d59c7e35c75cc2f59410e1cbfb2099b914dd574f
paulwagner <paul@typology.de>
2012-08-06 10:44:54 +0200

    Method test case for retrieval factory

--- Typology/test/de/typology/retrieval/AllTestsRetrieval.java
+++ Typology/test/de/typology/retrieval/AllTestsRetrieval.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PrimitiveRetrievalTest.class })
+@SuiteClasses({ PrimitiveRetrievalTest.class, RetrievalFactoryTest.class })
 public class AllTestsRetrieval {
 
 }
